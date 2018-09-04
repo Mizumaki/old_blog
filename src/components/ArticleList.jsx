7c256490e13ffa6f4e3001fb95d44b1e67016fe8
Mizumaki <statham.programming@gmail.com>
2018-09-04 17:15:08 +0900

    fix setState rendering bug

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -28,23 +28,26 @@ class Articlelist extends React.Component {
       .catch((err) => console.log("There has been error in fetch operation: ", err.message))
   }
 
-  componentDidUpdate() {
-    fetch(`https://ryota-mizumaki.com/api/article-list/${this.props.type}/${this.props.subType}?name=${this.props.query}`)
-      .catch((err) => { throw Error(err); }) // ネットワークなどのリクエスト以前のエラー処理
-      .then((res) => handleResponseErrors(res))
-      .then((res) => {
-        console.log('in fetch success');
-        return res.json();
-      })
-      .then((articles) => this.setState({ articles: articles }))
-      .catch((err) => console.log("There has been error in fetch operation: ", err.message))
+  componentDidUpdate(prevProps) {
+    // type, subtype is always equal, so handle update whether query is different or not
+    if (prevProps.query !== this.props.query) {
+      fetch(`https://ryota-mizumaki.com/api/article-list/${this.props.type}/${this.props.subType}?name=${this.props.query}`)
+        .catch((err) => { throw Error(err); }) // ネットワークなどのリクエスト以前のエラー処理
+        .then((res) => handleResponseErrors(res))
+        .then((res) => {
+          console.log('in fetch success');
+          return res.json();
+        })
+        .then((articles) => this.setState({ articles: articles }))
+        .catch((err) => console.log("There has been error in fetch operation: ", err.message))
+    } else {console.log('else')}
   }
 
   render() {
     const articles = this.state.articles
-    const blogCards = articles.map((article) => {
+    const blogCards = articles.map((article, i) => {
       return (
-        <div className={css(styles.wrap)}>
+        <div className={css(styles.wrap)} key={i}>
           <Link to={article.path}>
             <BlogCard title={article.title} subCategory={article.subCategory} lead={article.lead} date={article.date} />
           </Link>
