110c40d98dde6e9e5e087e8bbadeca90a87865fb
Mizumaki <statham.programming@gmail.com>
2018-09-29 10:47:22 +0900

    add list get api order by date

---
+++ functions/src/api/article-list/latest.js
@@ -0,0 +1,48 @@
+const url = require('url');
+const express = require('express');
+const router = express.Router();
+
+const firebase = require('../../firebase');
+const firestore = firebase.db;
+
+router.get('/', (req, res) => {
+  handleResponse(req, res);
+})
+
+const handleResponse = (req, res) => {
+  const reqParts = url.parse(req.url, true);
+  const count = reqParts.query.count
+  getArticleList(count)
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err))
+}
+
+const getArticleList = (count) => {
+  return new Promise((resolve, reject) => {
+    firestore.collection('articles').orderBy('date').limit(parseInt(count, 10)).get()
+      .then((querySnapshot) => {
+        let statham = [];
+        querySnapshot.forEach((doc) => {
+          data = doc.data();
+          const file_name = data.file_name
+          const title = data.header.title
+          const lead = data.header.lead
+          const mainCategory = data.category.main
+          const subCategory = data.category.sub
+          const date = data.update.toDate().toLocaleDateString('ja-JP').replace(/-/g, '/');
+          const json = {
+            path: `/${mainCategory}/${subCategory}/${file_name}`,
+            title: title,
+            lead: lead,
+            subCategory: subCategory,
+            date: date,
+          }
+          statham.push(json);
+        });
+        return resolve(statham);
+      })
+      .catch((err) => reject(err))
+  })
+}
+
+module.exports = router;
--- functions/src/api/article-list/routes.js
+++ functions/src/api/article-list/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/category', require('./category'));
 router.use('/tags', require('./tags'));
+router.use('/latest', require('./latest'));
 
 module.exports = router;
